ea1e751a30120824b93609e557c37d4b9882fd16
Russ Taylor <rssell.taylor@gmail.com>
2025-01-20 23:23:57 -0500

    update header and background

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
+import Header from "./Header.js";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -16,20 +16,6 @@ export const metadata = {
     title: "Russell Taylor",
     description: "Portfolio website for Russell Taylor",
 };
-
-function Header() {
-    return (
-        <header className="bg-slate-500 py-4">
-            <div className="container mx-auto px-4">
-                <nav className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <Link href="/">Home</Link>
-                    </div>
-                </nav>
-            </div>
-        </header>
-    )
-}
 
 export default function RootLayout({ children }) {
     return (
